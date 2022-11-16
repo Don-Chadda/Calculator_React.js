7b8e3a2965144ed9bd5c3458d1de82562d3e9313
Don-Chadda <peterturay81@gmail.com>
2022-11-16 18:37:28 +0000

    Worked on the equal and compute functions

---
+++ README 2.md
@@ -0,0 +1,2 @@
+# Calculator_React.js
+A project I worked on after learning React.js
--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -19,6 +19,7 @@ export default function Calculator() {
     }
 
     const handleAllClear = () => {
+
         setCurrent('')
         setPrevious('')
         setOperation('')
@@ -26,7 +27,7 @@ export default function Calculator() {
 
     const chooseOperation = (el) => {
         if(current === '') return
-        if(previous !== '') {
+        if(previous !== ''){
             let value = compute();
             setPrevious(value)
         } else {
@@ -38,7 +39,41 @@ export default function Calculator() {
 
     }
 
+    const equals = () => {
+        let value = compute();
+        if(value === undefined || value == null) return
+
+        setCurrent(value)
+        setPrevious('')
+        setOperation('')
+    }
+
     const compute = () => {
+
+        let result
+        let previousNumber = parseFloat(previous)
+        let currentNumber = parseFloat(current)
+
+        if(isNaN(previousNumber) || isNaN(currentNumber)) return
+
+        switch(operation) {
+            case '÷':
+                result = previousNumber / currentNumber;
+                break;
+            case '×':
+                result = previousNumber * currentNumber;
+                break;
+            case '+':
+                result = previousNumber + currentNumber;
+                break;
+            case '-':
+                result = previousNumber - currentNumber;
+                break;
+            default:
+                return
+        }
+
+        return result;
 
     }
 
@@ -54,7 +89,7 @@ export default function Calculator() {
            <Button data={'7'} onClick={appendValue}>7</Button>
            <Button data={'8'} onClick={appendValue}>8</Button>
            <Button data={'9'} onClick={appendValue}>9</Button>
-           <Button data={'x'} onClick={chooseOperation} operation>x</Button>
+           <Button data={'×'} onClick={chooseOperation} operation>×</Button>
            <Button data={'4'} onClick={appendValue}>4</Button>
            <Button data={'5'} onClick={appendValue}>5</Button>
            <Button data={'6'} onClick={appendValue}>6</Button>
@@ -65,7 +100,7 @@ export default function Calculator() {
            <Button data={'-'} onClick={chooseOperation} operation>-</Button>
            <Button data={'.'} period onClick={appendValue}>.</Button>
            <Button data={'0'} onClick={appendValue}>0</Button>
-           <Button gridSpan={2} equals>=</Button>
+           <Button gridSpan={2} onclick={equals} equals>=</Button>
         </Container>
     )
 
